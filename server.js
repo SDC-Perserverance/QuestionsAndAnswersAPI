d15bc17d8cb779a22b91c7cdc31e197f0df6c937
Daniel Ko <kod3349@gmail.com>
2021-03-18 09:13:16 -0500

    Setup simple Express server on port 3015

---
+++ server.js
@@ -0,0 +1,19 @@
+// Modules
+const express = require('express');
+const bodyParser = require('body-parser');
+// Server config
+const QnA =  express();
+const PORT = 3015;
+// Middleware
+QnA.use(bodyParser.json());
+// Route Handling
+QnA.get('/', (req, res) => {
+  res.send('Welcome to QnA express server');
+});
+
+
+// Listening to server
+QnA.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
+
